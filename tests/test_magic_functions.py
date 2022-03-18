b44377d4ad90a82bdf90755fbab96a40dc135d3e
JesusBandez <57400866+JesusBandez@users.noreply.github.com>
2022-03-18 15:43:27 -0400

    new tests for magic functions

--- tests/test_magic_functions.py
+++ tests/test_magic_functions.py
@@ -5,7 +5,7 @@ from utils.constants import *
 
 repl = StokhosCMD()
 test_cases, test_sol = [], []
-
+# -----------------------------  Pruebas .load ---------------------------
 # Archivo con instrucciones
 sol1 = ['OK: lex("x") ==> [TkId("x")]', 'OK: lex("true + 2") ==> [TkTrue, TkPlus, TkNumber(2)]', 'OK: lex("x ^ 2") ==> [TkId("x"), TkPower, TkNumber(2)]']
 test_cases.append(lambda :repl.default(r'.load tests\lexer\t_1.txt'))
@@ -24,7 +24,6 @@ test_sol.append(sol1)
 test_cases.append(lambda :repl.default(r'.load tests\lexer\t_4.txt'))
 test_sol.append(sol1 + sol2)
 
-
 cases = list(zip(test_cases, test_sol))
 @pytest.mark.parametrize("test_case,test_sol", cases)
 def test_load(test_case:str, test_sol:object, capsys):
@@ -37,6 +36,64 @@ def test_load(test_case:str, test_sol:object, capsys):
     for line in test_sol:
         repl.handle_output(line)
     captured2 = capsys.readouterr()
+
+    # Comprobar las salidas
+    assert captured1.out == captured2.out
+
+# ------------------------ Pruebas .failed y .reset -------------------
+
+def print_error_list_with_test_sol(test_sol:list):
+    repl.handle_output('[', RED)
+    for error in test_sol:
+        repl.handle_output(f'    ( {error} )', RED)
+    repl.handle_output(']', RED)
+    
+def str_invalid_char(c:str) -> str:
+    return f'Caracter inválido("{c}")'
+
+def str_invalid_Id(id:str) -> str:
+    return f'ID ilegal("{id}")'
+
+# Probar lista vacia
+repl = StokhosCMD()
+def test_empty_failed(capsys):
+    repl.default(r'.failed')
+    captured1 = capsys.readouterr()
+    print_error_list_with_test_sol([])
+    captured2 = capsys.readouterr()
+    assert captured1 == captured2
+
+
+test_cases, test_sol = [], []
+# Error de caracter invalido
+test_cases.append([r'.lex @'])
+test_sol.append([str_invalid_char('@')])
+
+# Comandos con errores enlazados
+test_cases.append([r'.lex 2', r'.lex $', r'.lex hola', r'.lex @'])
+test_sol.append([str_invalid_char('$'), str_invalid_char('@')])
+
+
+cases = list(zip(test_cases, test_sol))
+@pytest.mark.parametrize("test_case,test_sol", cases)
+def test_failed(test_case:str, test_sol:object, capsys):
+
+    # Ejecutar los comandos en el caso prueba
+    for command in test_case:
+        repl.default(command)
+    # No importan los prints ejecutados
+    capsys.readouterr()
+    # Mostrar la lista de errores y capturarla
+    repl.default(r'.failed')
+    captured1 = capsys.readouterr()
+
+    # Imprimir la solucion esperada y capturarla
+    print_error_list_with_test_sol(test_sol)
+    captured2 = capsys.readouterr()
+
+    # Reiniciar la lista de errores y despejar salida estandar
+    repl.default(r'.reset')
+    capsys.readouterr()
 
     # Comprobar las salidas
     assert captured1.out == captured2.out
