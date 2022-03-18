46cee2d730ddf57b959f370e4fd376a0831b6188
JesusBandez <57400866+JesusBandez@users.noreply.github.com>
2022-03-18 12:14:14 -0400

    Added parentheses and quoted tests

--- tests/test_grammar_complicate_expresions.py
+++ tests/test_grammar_complicate_expresions.py
@@ -1,5 +1,6 @@
 """Modulo de pruebas para la precedencia de los operadores de las reglas definidas en grammar.py"""
 
+from ast import Num
 import os, sys, pytest
 
 sys.path.insert(1, os.path.abspath('.'))
@@ -33,13 +34,24 @@ test_cases.extend(list((f"{varType} x := {plus_or_minus_unary}2{plus_or_minus}3 
 test_sol.extend(list((SymDef(Type(varType), Id("x"), Comparison(comparison, BinOp(plus_or_minus, UnOp(plus_or_minus_unary, Number(2)), Number(3)), BinOp(prod_div_pow, Number(2), UnOp(plus_or_minus_unary, Number(5))) )) for plus_or_minus_unary in NUM_UN_OPS for plus_or_minus in NUM_UN_OPS for comparison in COMPARISONS for prod_div_pow in ["^", "*", "/"] for varType in VAR_TYPE)))
 
 # Asignaciones de arreglos
-test_cases.extend(list(( f'array := [2 {binNumOp} {unOp}3, z {comparison} false {bool_bin_op} x, h[2 {binNumOp} {unOp}3]];' for binNumOp in NUM_BIN_OPS for unOp in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS )))
-test_sol.extend(list((AssignArray(Id('array'), ElemList(None).__debug_Init__([BinOp(binNumOp, Number(2), UnOp(unOp, Number(3))), BinOp (bool_bin_op, Comparison(comparison, Id('z'), Boolean('false')), Id('x') ), ArrayAccess(Id('h'),BinOp(binNumOp, Number(2), UnOp(unOp, Number(3))) )])) for binNumOp in NUM_BIN_OPS for unOp in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS)))
+test_cases.extend(list(( f'array := [2 {bin_num_op} {un_op}3, z {comparison} false {bool_bin_op} x, h[2 {bin_num_op} {un_op}3]];' for bin_num_op in NUM_BIN_OPS for un_op in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS )))
+test_sol.extend(list((AssignArray(Id('array'), ElemList(None).__debug_Init__([BinOp(bin_num_op, Number(2), UnOp(un_op, Number(3))), BinOp (bool_bin_op, Comparison(comparison, Id('z'), Boolean('false')), Id('x') ), ArrayAccess(Id('h'),BinOp(bin_num_op, Number(2), UnOp(un_op, Number(3))) )])) for bin_num_op in NUM_BIN_OPS for un_op in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS)))
 
 # Definiciones de arreglos
-test_cases.extend(list((f'[{arr_type}] array := [2 {binNumOp} {unOp}3, z {comparison} false {bool_bin_op} x, h[2 {binNumOp} {unOp}3]];' for binNumOp in NUM_BIN_OPS for unOp in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS for arr_type in VAR_TYPE )))
-test_sol.extend(list((SymDef(Type(TypeArray(Type(arr_type))), Id('array'), ElemList(None).__debug_Init__([BinOp(binNumOp, Number(2), UnOp(unOp, Number(3))), BinOp (bool_bin_op, Comparison(comparison, Id('z'), Boolean('false')), Id('x') ), ArrayAccess(Id('h'),BinOp(binNumOp, Number(2), UnOp(unOp, Number(3))) )])) for binNumOp in NUM_BIN_OPS for unOp in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS for arr_type in VAR_TYPE)))
+test_cases.extend(list((f'[{arr_type}] array := [2 {bin_num_op} {un_op}3, z {comparison} false {bool_bin_op} x, h[2 {bin_num_op} {un_op}3]];' for bin_num_op in NUM_BIN_OPS for un_op in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS for arr_type in VAR_TYPE )))
+test_sol.extend(list((SymDef(Type(TypeArray(Type(arr_type))), Id('array'), ElemList(None).__debug_Init__([BinOp(bin_num_op, Number(2), UnOp(un_op, Number(3))), BinOp (bool_bin_op, Comparison(comparison, Id('z'), Boolean('false')), Id('x') ), ArrayAccess(Id('h'),BinOp(bin_num_op, Number(2), UnOp(un_op, Number(3))) )])) for bin_num_op in NUM_BIN_OPS for un_op in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS for arr_type in VAR_TYPE)))
 
+# Asignaciones con expresiones usando parentesis
+test_cases.extend(list((f"x := 3 {prod_div_pow} ({plus_or_minus_unary}2{plus_or_minus}3) {comparison} 2{prod_div_pow}{plus_or_minus_unary} 5;" for plus_or_minus_unary in NUM_UN_OPS for plus_or_minus in NUM_UN_OPS for comparison in COMPARISONS for prod_div_pow in ["^", "*", "/"])))
+test_sol.extend(list((Assign(Id("x"), Comparison(comparison, BinOp(prod_div_pow, Number(3), Parentheses(BinOp(plus_or_minus, UnOp(plus_or_minus_unary, Number(2)), Number(3)))), BinOp(prod_div_pow, Number(2), UnOp(plus_or_minus_unary, Number(5))) )) for plus_or_minus_unary in NUM_UN_OPS for plus_or_minus in NUM_UN_OPS for comparison in COMPARISONS for prod_div_pow in ["^", "*", "/"])))
+
+# Asignaciones con acotamiento
+test_cases.extend(list((f"x := 3 {prod_div_pow} '{plus_or_minus_unary}2{plus_or_minus}3' {comparison} 2{prod_div_pow}{plus_or_minus_unary} 5;" for plus_or_minus_unary in NUM_UN_OPS for plus_or_minus in NUM_UN_OPS for comparison in COMPARISONS for prod_div_pow in ["^", "*", "/"])))
+test_sol.extend(list((Assign(Id("x"), Comparison(comparison, BinOp(prod_div_pow, Number(3), Quoted(BinOp(plus_or_minus, UnOp(plus_or_minus_unary, Number(2)), Number(3)))), BinOp(prod_div_pow, Number(2), UnOp(plus_or_minus_unary, Number(5))) )) for plus_or_minus_unary in NUM_UN_OPS for plus_or_minus in NUM_UN_OPS for comparison in COMPARISONS for prod_div_pow in ["^", "*", "/"])))
+
+# Definiciones de arreglos con funciones, parentesis y acotamiento
+test_cases.extend(list((f'[{arr_type}] array := [2 {bin_num_op} (({un_op}3) {bin_num_op} 7), z {comparison} false {bool_bin_op} x, h[2 {bin_num_op} {un_op}3]];' for bin_num_op in NUM_BIN_OPS for un_op in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS for arr_type in VAR_TYPE )))
+test_sol.extend(list((SymDef(Type(TypeArray(Type(arr_type))), Id('array'), ElemList(None).__debug_Init__([BinOp(bin_num_op, Number(2), Parentheses(BinOp(bin_num_op, Parentheses(UnOp(un_op, Number(3))), Number(7))))   , BinOp (bool_bin_op, Comparison(comparison, Id('z'), Boolean('false')), Id('x') ), ArrayAccess(Id('h'),BinOp(bin_num_op, Number(2), UnOp(un_op, Number(3))) )])) for bin_num_op in NUM_BIN_OPS for un_op in NUM_UN_OPS for comparison in COMPARISONS for bool_bin_op in BOOL_BIN_OPS for arr_type in VAR_TYPE)))
 
 
 # ------------ Ejecucion de pruebas ---------------
