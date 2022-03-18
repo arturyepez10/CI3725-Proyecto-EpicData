aa6c2d6bc3302a6a1ec8a8719036fd7b26a785ee
JesusBandez <57400866+JesusBandez@users.noreply.github.com>
2022-03-18 18:09:29 -0400

    Update test_magic_functions.py

--- tests/test_magic_functions.py
+++ tests/test_magic_functions.py
@@ -8,20 +8,20 @@ test_cases, test_sol = [], []
 # -----------------------------  Pruebas .load ---------------------------
 # Archivo con instrucciones
 sol1 = ['OK: lex("x") ==> [TkId("x")]', 'OK: lex("true + 2") ==> [TkTrue, TkPlus, TkNumber(2)]', 'OK: lex("x ^ 2") ==> [TkId("x"), TkPower, TkNumber(2)]']
-test_cases.append(lambda :repl.default(r'.load tests\lexer\t_1.txt'))
+test_cases.append(lambda :repl.default(r'.load tests\tests_load\t_1.txt'))
 test_sol.append(sol1)
 
 # Archivo con instrucciones y lineas en blanco
 sol2 = ['OK: lex("false || true") ==> [TkFalse, TkOr, TkTrue]', 'OK: lex("32") ==> [TkNumber(32)]', 'OK: lex("algoAlejado") ==> [TkId("algoAlejado")]']
-test_cases.append(lambda :repl.default(r'.load tests\lexer\t_2.txt'))
+test_cases.append(lambda :repl.default(r'.load tests\tests_load\t_2.txt'))
 test_sol.append(sol2)
 
 # Llamar al primer archivo con otro
-test_cases.append(lambda :repl.default(r'.load tests\lexer\t_3.txt'))
+test_cases.append(lambda :repl.default(r'.load tests\tests_load\t_3.txt'))
 test_sol.append(sol1)
 
 # Llamar a los dos primeros archivos
-test_cases.append(lambda :repl.default(r'.load tests\lexer\t_4.txt'))
+test_cases.append(lambda :repl.default(r'.load tests\tests_load\t_4.txt'))
 test_sol.append(sol1 + sol2)
 
 cases = list(zip(test_cases, test_sol))
@@ -42,10 +42,13 @@ def test_load(test_case:str, test_sol:object, capsys):
 
 # ------------------------ Pruebas .failed y .reset -------------------
 
-def print_error_list_with_test_sol(test_sol:list):
+def print_error_list_with_test_sol(test_sol):
     repl.handle_output('[', RED)
     for error in test_sol:
-        repl.handle_output(f'    ( {error} )', RED)
+        if isinstance(error, dict):
+            repl.handle_output(f'    ( {error["error"]}, archivo "{error["file"]}", linea {error["line"]} )', RED)
+        else:
+            repl.handle_output(f'    ( {error} )', RED)
     repl.handle_output(']', RED)
     
 def str_invalid_char(c:str) -> str:
@@ -73,6 +76,9 @@ test_sol.append([str_invalid_char('@')])
 test_cases.append([r'.lex 2', r'.lex $', r'.lex hola', r'.lex @'])
 test_sol.append([str_invalid_char('$'), str_invalid_char('@')])
 
+# .load que contiene errores
+test_cases.append([r'.load tests\tests_load\t_error_1.txt'])
+test_sol.append([{'error' : str_invalid_char('@'), 'file': 't_error_1.txt', 'line': 1}])
 
 cases = list(zip(test_cases, test_sol))
 @pytest.mark.parametrize("test_case,test_sol", cases)
