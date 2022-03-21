a9c23f31bd8618499d51cb8aaa5f9a3934517b1a
chrischriscris <18-10892@usb.ve>
2022-03-21 17:42:21 -0400

    import deleted

--- REPL.py
+++ REPL.py
@@ -19,7 +19,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
 from cmd import Cmd
-from distutils.log import error
 from textwrap import dedent
 from typing import Union
 
