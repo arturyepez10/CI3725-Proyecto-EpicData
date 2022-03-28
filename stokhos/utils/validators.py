9ffff963371bf996de8dccd015410ae0333598da
chrischriscris <18-10892@usb.ve>
2022-03-28 19:37:56 -0400

    avances validadores del ast de stokhos

--- stokhos/utils/validators.py
+++ stokhos/utils/validators.py
@@ -37,28 +37,30 @@ def validate_ast(ast: AST.AST, sym_table: dict) -> AST.AST:
     try:
         if type(ast) == AST.SymDef:
             # Caso 1: Es una definición de variable
-            return validate_vardef(ast, sym_table)
+            validate_vardef(ast, sym_table)
         elif type(ast) == AST.Assign:
             # Caso 2.1: Es una asignación
-            return validate_assign(ast)
+            validate_assign(ast)
         elif type(ast) == AST.AssignArrayElement:
             # Caso 2.1: Es una asignación de un acceso a arreglo
-            return AST.Error('TODO')
+            AST.Error('TODO')
         else:
             # Caso 3: Es una expresión
-            return validate_expr(ast)
+            validate_expr(ast)
     except SemanticError as e:
         return AST.Error(e.message)
+    
+    return ast
 
-def validate_vardef(ast: AST.SymDef, sym_table: dict) -> AST.AST:
+def validate_vardef(ast: AST.SymDef, sym_table: dict) -> None:
     # Se valida que la variable no haya sido previamente definida
     if ast.id in sym_table:
         raise SemanticError(f'Variable {ast.id} ya definida')
 
-    # Se validan los tipos de las variables
-    return validate_type(ast.value, sym_table)
+    # Se valida el tipo del lado derecho de la asignación
+    validate_type(ast.type, ast.value, sym_table)
 
-def validate_assign(ast: AST.Assign, sym_table: dict) -> AST.AST:
+def validate_assign(ast: AST.Assign, sym_table: dict) -> None:
     # Se valida que la variable a la que se le asigna sea previamente
     # definida
     if ast.name not in sym_table:
@@ -71,14 +73,14 @@ def validate_assign(ast: AST.Assign, sym_table: dict) -> AST.AST:
         else:
             return ast
 
-def validate_expr(ast: AST.AST, sym_table: dict) -> AST.AST:
+def validate_expr(ast: AST.AST, sym_table: dict) -> None:
     # Se valida que la expresión sea de tipo booleano
     if ast.type != 'bool':
         raise Exception(f'Expresión no es de tipo booleano')
     else:
         return ast
 
-def validate_type(ast: AST.AST, sym_table: dict) -> AST.AST:
+def validate_type(ast: AST.AST, sym_table: dict) -> None:
     # Se valida que la expresión sea de tipo booleano
     if ast.type != 'bool':
         raise Exception(f'Expresión no es de tipo booleano')
