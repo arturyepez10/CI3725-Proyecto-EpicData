c62880d52883e677c806d0d5afc0077c28f54b93
chrischriscris <18-10892@usb.ve>
2022-04-09 21:23:58 -0400

    reordered declarations and type annotations added

--- stokhos/AST.py
+++ stokhos/AST.py
@@ -16,11 +16,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+from typing import Union
 from math import floor
 
 from .utils.constants import *
 from .utils.custom_exceptions import *
-
 
 class AST:
     def __repr__(self) -> str:
@@ -40,10 +40,10 @@ class Terminal(AST):
         return False
 
 class Number(Terminal):
-    def __init__(self, value: object):
+    def __init__(self, value: int):
         self.type = NUM
         self.value = value
-        
+
     # Sobrecarga de operadores para números de Stókhos
     def __add__(self, other):
         return Number(self.value + other.value)
@@ -53,10 +53,10 @@ class Number(Terminal):
     
     def __mul__(self, other):
         return Number(self.value * other.value)
-    
+
     def __truediv__(self, other):
         return Number(self.value / other.value)
-    
+
     def __mod__(self, other):
         return Number(self.value % other.value)
     
@@ -65,19 +65,19 @@ class Number(Terminal):
 
     def __neg__(self):
         return Number(-self.value)
-    
+
     def __pos__(self):
         return self
-    
+
     def __lt__(self, other):
         return Boolean(self.value < other.value)
 
     def __le__(self, other):
         return Boolean(self.value <= other.value)
-    
+
     def __gt__(self, other):
         return Boolean(self.value > other.value)
-    
+
     def __ge__(self, other):
         return Boolean(self.value >= other.value)
 
@@ -85,42 +85,69 @@ class Number(Terminal):
         return Number(floor(self.value))
 
 class Boolean(Terminal):
-    def __init__(self, value: object):
+    def __init__(self, value: bool):
         self.type = NUM
         self.value = value
 
     def __str__(self) -> str:
-        return f'{self.value.__str__().lower()}'
+        return super().__str__().lower()
 
     # Sobrecarga de operadores para Boolean de Stókhos
     def __bool__(self):
         return self.value
-        
+
 class Id(Terminal):
-    def __init__(self, value: object):
+    def __init__(self, value: str):
         self.type = None
         self.value = value
-    pass
+
+# -------- TIPOS --------
+class TypedArray(AST):
+    def __init__(self, _type: str):
+        self.type = _type
+
+    def __str__(self) -> str:
+        return f'[{self.type}]'
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, type(self)):
+            # Hace que un arreglo vacío pase por bool o num según sea necesario
+            if self.type == 'void' or other.type== 'void':
+                return True
+            return self.type == other.type
+        return False
+
+class Type(AST):
+    def __init__(self, _type: Union[TypedArray, str]):
+        self.type = _type
+
+    def __str__(self) -> str:
+        return f'{self.type.__str__()}'
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, type(self)):
+            return self.type == other.type
+        return False
 
 # -------- OPERACIONES BINARIAS --------
 class BinOp(AST):
-    def __init__(self, op: str, lhs_term: object, rhs_term: object):
+    def __init__(self, op: str, lhs: AST, rhs: AST):
         self.type = None
         self.op = op
-        self.lhs_term = lhs_term
-        self.rhs_term = rhs_term
+        self.lhs = lhs
+        self.rhs = rhs
 
     def __str__(self) -> str:
-        return f'{self.lhs_term} {self.op} {self.rhs_term}'
+        return f'{self.lhs} {self.op} {self.rhs}'
 
     def ast2str(self) -> str:
-        return f'({self.lhs_term.ast2str()} {self.op} {self.rhs_term.ast2str()})'
+        return f'({self.lhs.ast2str()} {self.op} {self.rhs.ast2str()})'
 
     def __eq__(self, other):
         if isinstance(other, type(self)):
             return (self.op == other.op
-                and self.lhs_term == other.lhs_term 
-                and self.rhs_term == other.rhs_term)
+                and self.lhs == other.lhs 
+                and self.rhs == other.rhs)
         return False
 
 class Comparison(BinOp):
@@ -128,7 +155,7 @@ class Comparison(BinOp):
 
 # -------- OPERACIONES UNARIAS --------
 class UnOp(AST):
-    def __init__(self, op: str, term: object):
+    def __init__(self, op: str, term: AST):
         self.type = None
         self.op = op
         self.term = term
@@ -145,130 +172,34 @@ class UnOp(AST):
                 and self.term == other.term)
         return False
 
-# -------- TIPOS --------
-class Type(AST):
-    def __init__(self, _type: object):
-        self.type = _type
-
-    def __str__(self) -> str:
-        return f'{self.type.__str__()}'
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, type(self)):
-            return self.type == other.type
-        return False
-
-class TypeArray(AST):
-    def __init__(self, _type: object):
-        self.type = _type        
-
-    def __str__(self) -> str:
-        return f'[{self.type}]'
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, type(self)):
-            # Hace que un arreglo vacío pase por bool o num según sea necesario
-            if self.type.type == 'void' or other.type.type == 'void':
-                return True
-            return self.type == other.type
-        return False
-
-class PrimitiveType(AST):
-    def __init__(self, type: object):
-        self.type = type        
-
-    def __str__(self) -> str:
-        return f'{self.type}'
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, type(self)):
-            return self.type == other.type
-        return False
-
-# -------- DEFINICIONES --------
-class SymDef(AST):
-    def __init__(self, _type: Type, _id: object, rhs_expr: object):
-        self.type = _type
-        self.id = _id
-        self.rhs_expr = rhs_expr
-        
-    def ast2str(self) -> str:
-        return f'SymDef({self.type}, {self.id}, {self.rhs_expr.ast2str()})'
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, type(self)):
-            return (self.type == other.type
-                and self.id == other.id
-                and self.rhs_expr == other.rhs_expr)
-        return False
-
-# -------- ASIGNACIONES --------
-class Assign(AST):
-    def __init__(self, _id: object, rhs_expr: object):
-        self.id = _id
-        self.rhs_expr = rhs_expr
-
-    def ast2str(self) -> str:
-        return f'Assign({self.id}, {self.rhs_expr.ast2str()})'
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, type(self)):
-            return (self.id == other.id
-                and self.rhs_expr == other.rhs_expr)
-        return False
-
-class AssignArrayElement(AST):
-    def __init__(self, array_access: object, rhs_expr: object):
-        self.array_access = array_access
-        self.rhs_expr = rhs_expr
-
-    def ast2str(self) -> str:
-        return f'Assign({self.array_access}[{self.index.ast2str()}], {self.rhs_expr.ast2str()})'
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, type(self)):
-            return (self.array_access == other.array_access
-                and self.rhs_expr == other.rhs_expr)
-        return False
-
-    def execute(self, symbol_table: dict) -> str:        
-        
-        # Evaluar el indice y lado derecho
-        index = self.index.evaluate(symbol_table)        
-        val = self.rhs_expr.evaluate(symbol_table)
-
-        try:
-            symbol_table[self.id.value].value[index.value].value = val.value
-        except IndexError:
-            raise RuntimeError(f'El indice {index.value} no está dentro del rango '
-                f'de la variable {self.id.value}')
-        except TypeError:
-            raise RuntimeError(f'Se esperaba un índice entero, pero se '
-                f'obtuvo {index.value}')
-
-        return f'{self.id.value}[{index.value}] := {val.value}'
-
 # -------- OTRAS EXPRESIONES --------
 class Quoted(AST):
-    def __init__(self, expr: object):
+    def __init__(self, expr: AST):
         self.type = None
         self.expr = expr
 
     def __str__(self) -> str:
         return f"'{self.expr}'"
-        
+
+    def ast2str(self) -> str:
+        return f"'{self.expr.ast2str()}'"
+
     def __eq__(self, other: object) -> bool:
         if isinstance(other, type(self)):
             return self.expr == other.expr
         return False
 
 class Array(AST):
-    def __init__(self, elements: list[object]):
+    def __init__(self, elements: list[AST]):
         self.type = None
         self.elements = elements
 
     def __str__(self) -> str:
         return f'{self.elements}'
+
+    def ast2str(self) -> str:
+        els_str = ', '.join([el.ast2str() for el in self.elements])
+        return f'[{els_str}]'
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, type(self)):
@@ -279,26 +210,11 @@ class Array(AST):
     def __len__(self) -> int:
         return len(self.elements)
 
-    def __getitem__(self, index: int) -> object:
+    def __getitem__(self, index: int) -> AST:
         return self.elements[index]
 
-class ArrayAccess(AST):
-    def __init__(self, id: object, _index:object):
-        self.type = None
-        self.id = id
-        self.index = _index
-
-    def __str__(self) -> str:
-        return f'{self.id}[{self.index}]'
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, type(self)):
-            return (self.id == other.id
-                and self.index == other.index)
-        return False
-
-class Function(AST):
-    def __init__(self, _id: object, _args: list[object]):
+class FunctionCall(AST):
+    def __init__(self, _id: Id, _args: list[AST]):
         self.type = None
         self.id = _id
         self.args = _args
@@ -307,25 +223,90 @@ class Function(AST):
         args_str = f'{self.args}'
         return f'{self.id}({args_str[1:-1]})'
 
+    def ast2str(self) -> str:
+        args_str = ', '.join([arg.ast2str() for arg in self.args])
+        return f'{self.id}({args_str})'
+
     def __eq__(self, other: object) -> bool:
         if isinstance(other, type(self)):
             return (self.id == other.id
                 and self.args == other.args)
         return False
 
+class ArrayAccess(AST):
+    def __init__(self, expr: Union[Id, FunctionCall, Array], _index: AST):
+        self.type = None
+        self.expr = expr # Puede ser una id, un arreglo o una función
+        self.index = _index
+
+    def __str__(self) -> str:
+        return f'{self.expr}[{self.index}]'
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, type(self)):
+            return (self.expr == other.expr
+                and self.index == other.index)
+        return False
+
+# -------- DEFINICIONES --------
+class SymDef(AST):
+    def __init__(self, _type: Type, _id: Id, rhs: AST):
+        self.type = _type
+        self.id = _id
+        self.rhs = rhs
+
+    def ast2str(self) -> str:
+        return f'SymDef({self.type}, {self.id}, {self.rhs.ast2str()})'
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, type(self)):
+            return (self.type == other.type
+                and self.id == other.id
+                and self.rhs == other.rhs)
+        return False
+
+# -------- ASIGNACIONES --------
+class Assign(AST):
+    def __init__(self, _id: Id, rhs: AST):
+        self.id = _id
+        self.rhs = rhs
+
+    def ast2str(self) -> str:
+        return f'Assign({self.id}, {self.rhs.ast2str()})'
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, type(self)):
+            return (self.id == other.id
+                and self.rhs == other.rhs)
+        return False
+
+class AssignArrayElement(AST):
+    def __init__(self, array_access: ArrayAccess, rhs: object):
+        self.array_access = array_access
+        self.rhs = rhs
+
+    def ast2str(self) -> str:
+        return f'Assign({self.array_access}[{self.index.ast2str()}], {self.rhs.ast2str()})'
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, type(self)):
+            return (self.array_access == other.array_access
+                and self.rhs == other.rhs)
+        return False
+
 # --- AST DE ERROR ---
 class Error(AST):
     def __init__(self, cause: str):
         self.cause = cause
-    
+
     def __str__(self) -> str:
-        return f'''Error('{self.cause}')'''
+        return f"Error('{self.cause}')"
 
 # Alias para los tipos de datos
-NUM = Type(PrimitiveType('num'))
-BOOL = Type(PrimitiveType('bool'))
-VOID = Type(PrimitiveType('void'))
-NUM_ARRAY = Type(TypeArray(PrimitiveType('num')))
-BOOL_ARRAY = Type(TypeArray(PrimitiveType('bool')))
+NUM = Type('num')
+BOOL = Type('bool')
+VOID = Type('void')
+NUM_ARRAY = Type(TypedArray('num'))
+BOOL_ARRAY = Type(TypedArray('bool'))
 # De utilidad para 'inferencia de tipos'
-VOID_ARRAY = Type(TypeArray(PrimitiveType('void')))
+VOID_ARRAY = Type(TypedArray('void'))
